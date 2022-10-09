54d1b2314c53cb1df329a1fe37ff1e05633b9097
Jagan <jagannathap@gmail.com>
2022-10-10 02:04:07 +0530

    setting initial project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3b2e30f485msh7295a39662c1bbcp1d389ajsn585d9587f47d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="5e23e58839acbd53f733091dafa621f7";
